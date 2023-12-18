4a3213cbeb8f64532fd360c76faa34eb12bc541f
Maxwell <lubalecliff@gmail.com>
2023-12-18 15:51:30 +0300

    Completed Assignment

---
+++ app/migrations/versions/3d78b874e116_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3d78b874e116
+Revises: 8a257a59e6b3
+Create Date: 2023-12-18 15:40:24.025824
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3d78b874e116'
+down_revision = '8a257a59e6b3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ app/migrations/versions/8a257a59e6b3_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 8a257a59e6b3
+Revises: 
+Create Date: 2023-12-18 15:33:55.367796
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8a257a59e6b3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
